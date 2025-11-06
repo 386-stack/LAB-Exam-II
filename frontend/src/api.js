8d4bed332062f42c2a01067b1733f0e397642eda
Naina <2300030386cseh1@gmail.com>
2025-11-06 14:36:30 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 /*
 
-const USER_API_URL = "http://localhost:9096/api/users";
+const USER_API_URL = "http://localhost:9096/api/users"; 
 const QUIZ_API_URL = "http://localhost:9096/api/quizzes";
 
 /* ---------------- USER APIs ---------------- */
